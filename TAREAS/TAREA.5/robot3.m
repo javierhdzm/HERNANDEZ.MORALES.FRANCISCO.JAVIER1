806f20d72b9982dfdce0e907e6d6b12d0bdebc46
javierhdzm <46731169+javierhdzm@users.noreply.github.com>
2019-04-11 13:00:09 -0500

    Add files via upload

---
+++ TAREAS/TAREA.5/robot3.m
@@ -0,0 +1,11 @@
+syms theta1
+syms theta2
+syms theta3
+syms d2
+T1=[cos(theta1),-sin(theta1),0,0;0,0,1,0;-sin(theta1),-cos(theta1),0,0;0,0,0,1]
+syms L1
+T2=[cos(theta2),-sin(theta2),0,L1;0,0,-1,-d2;-sin(theta2),-cos(theta2),0,0;0,0,0,1]
+syms L2
+T3=[cos(theta3),-sin(theta3),0,L2;0,0,1,0;-sin(theta3),-cos(theta3),0,0;0,0,0,1]
+syms ans
+ans =T1*T2*T3
---
+++ TAREAS/TAREA.5/robot5.m
@@ -0,0 +1,11 @@
+syms theta1
+syms theta2
+syms theta3
+syms d1
+syms d2
+T1=[cos(theta1),-sin(theta1),0,0;sin(theta1),cos(theta1),0,0;0,0,1,d1;0,0,0,1]
+syms L1
+T2=[cos(theta2),-sin(theta2),0,0;0,0,-1,d2;cos(theta2),sin(theta2),0,0;0,0,0,1]
+T3=[cos(theta3),-sin(theta3),0,L1;sin(theta3),cos(theta3),0,0;0,0,1,0;0,0,0,1]
+syms ans
+ans =T1*T2*T3
